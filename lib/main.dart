c6ebc6ec02bd7c447f1c78fa78b0b3b6410a592e
Nguyễn Minh Duy <217141310+duyxyz@users.noreply.github.com>
2026-03-22 16:23:22 +0700

    chuyển qua flutter

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebAppScreen(),
+    );
+  }
+}
+
+class WebAppScreen extends StatefulWidget {
+  const WebAppScreen({super.key});
+
+  @override
+  State<WebAppScreen> createState() => _WebAppScreenState();
+}
+
+class _WebAppScreenState extends State<WebAppScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..loadRequest(Uri.parse('https://www.example.com/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if (didPop) return;
+        if (await controller.canGoBack()) {
+          controller.goBack();
+        } else {
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: WebViewWidget(controller: controller),
+        ),
+      ),
+    );
+  }
+}
